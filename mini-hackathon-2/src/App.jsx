c6b0ad6008100a6bec4a2e72f126afc172d04ca0
kurealnum <cattledoger@gmail.com>
2024-07-16 11:43:47 -0400

    Basic function to add to list

--- mini-hackathon-2/src/App.jsx
+++ mini-hackathon-2/src/App.jsx
@@ -3,20 +3,33 @@ import "./App.css";
 
 function App() {
   const [code, setCode] = useState();
+  const [consumed, setConsumed] = useState([]);
 
   function getData() {
     const fields = "energy_serving";
     const response = request(code, fields);
-    response.then((it) => console.log(it));
+    response.then((it) => {
+      setConsumed([...consumed, code]);
+    });
   }
 
   return (
     <>
-      <label>Input a barcode</label>
-      <input onChange={(e) => setCode(e.target.value)}></input>
-      <button type="submit" onClick={getData}>
-        Submit
-      </button>
+      <h1>Consumption Tracker</h1>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <label>Input a barcode</label>
+        <input onChange={(e) => setCode(e.target.value)}></input>
+        <button type="submit" onClick={getData}>
+          Submit
+        </button>
+      </form>
+      <div id="listed-items">
+        {consumed.length == 0 ? (
+          <p>You have not entered any items yet!</p>
+        ) : (
+          consumed.map((item) => <p key={item}>{item}</p>)
+        )}
+      </div>
     </>
   );
 }
